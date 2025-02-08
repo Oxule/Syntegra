57289e123bdcddb19e48de24b412a1ecd231118f
Oxule <ribb2017@mail.ru>
2025-02-09 01:01:21 +0300

    Frontend: Poor Projects page done

--- frontend/src/Projects.jsx
+++ frontend/src/Projects.jsx
@@ -1,10 +1,24 @@
 function Projects() {
 
-  return (
-    <h1>
-      Hi!
-    </h1>
-  )
+    function Project({info}){
+        return <a className={"projects-card button-raw"} style={{backgroundColor: "red"}}>
+            Some card data
+        </a>
+    }
+
+    return (
+        <div className={"projects-grid"}>
+            <Project info={{
+                name: "some project",
+                description: "description",
+                createdAt: "09-02-2025"
+            }}/>
+            <Project/>
+            <Project/>
+            <Project/>
+            <Project/>
+        </div>
+    )
 }
 
 export default Projects
