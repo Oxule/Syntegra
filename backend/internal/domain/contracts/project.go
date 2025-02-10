9fbc1ba62e6ba6efd2af7fa8bc117245847fbad7
msakp <voilond@mail.ru>
2025-02-10 19:36:33 +0300

    Backend: contracts fixed: error return type switched to *dto.HttpErr

--- backend/internal/domain/contracts/project.go
+++ backend/internal/domain/contracts/project.go
@@ -8,11 +8,11 @@ import (
 )
 
 type ProjectService interface {
-	Create(ctx context.Context, p dto.ProjectCreate) (dto.ProjectView, error)
-	Edit(ctx context.Context, p dto.ProjectCreate) dto.ProjectView
-	ListMembers(ctx context.Context, projectId uuid.UUID) []dto.ProjectMember
-	Invite(ctx context.Context, projectId uuid.UUID, username string) error
-	UnInvite(ctx context.Context, projectId uuid.UUID, username string) error
+	Create(ctx context.Context, p dto.ProjectCreate) (dto.ProjectView, *dto.HttpErr)
+	Edit(ctx context.Context, p dto.ProjectCreate) (dto.ProjectView, *dto.HttpErr)
+	ListMembers(ctx context.Context, projectId uuid.UUID) ([]dto.ProjectMember, *dto.HttpErr)
+	Invite(ctx context.Context, projectId uuid.UUID, username string) *dto.HttpErr
+	UnInvite(ctx context.Context, projectId uuid.UUID, username string) *dto.HttpErr
 }
 
 type ProjectRepository interface{}
