4d1eb58811a6c92aaa2555791b233491188a0cd9
Oxule <ribb2017@mail.ru>
2025-02-10 15:38:48 +0300

    Frontend: added sample schemes

--- frontend/src/Project/Project.jsx
+++ frontend/src/Project/Project.jsx
@@ -15,6 +15,36 @@ export function Project() {
     const lorem30 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem corporis cumque deleniti dolore earum, eius illo iste iure, officiis placeat quas quisquam quo ratione repellat reprehenderit saepe sequi, temporibus voluptate!";
     const lorem60 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis earum et expedita minima nam necessitatibus nulla quos voluptatibus? Accusantium assumenda aut cumque debitis delectus eligendi eveniet explicabo facere hic illum ipsum minus modi nam neque obcaecati placeat, quia reprehenderit repudiandae saepe similique suscipit tempora tenetur ut veniam voluptatibus. Aspernatur est id incidunt magnam nobis pariatur quaerat sequi sint voluptate voluptatum.";
 
+    const schemes = [
+        {
+            id: "1",
+            name: "createUser",
+            fields:[
+                {
+                    name: "name",
+                    type: "string",
+                    description: "length [3-64]"
+                },
+                {
+                    name: "password",
+                    type: "string",
+                    description: "length [8-64], special regex"
+                }
+            ]
+        },
+        {
+            id: "2",
+            name: "id",
+            fields: [
+                {
+                    name: "this",
+                    description: "just UUID",
+                    type: "string",
+                }
+            ]
+        }
+    ]
+
     const endpoints = [
         {
             id: "1",
@@ -81,7 +111,7 @@ export function Project() {
             <Text ta={"center"} c="dimmed">{projectInfo.createdAt}</Text>
             <Text ta={"center"} size={"lg"} style={{marginBottom: "32px"}}>{projectInfo.description}</Text>
             {/*TODO: севочка, добавь кнопку редактирования тоже через dialogwindow*/}
-            <Endpoints endpoints={endpoints}/>
+            <Endpoints endpoints={endpoints} schemes={schemes}/>
         </>
     );
 }
